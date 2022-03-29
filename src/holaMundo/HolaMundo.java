61af617d8f3537acc9870c50c2372f6ec4c3d653
Isidro <alum.isidro.linares.camarena@ieslosremedios.org>
2022-03-29 14:53:29 +0200

    Segundo commit

--- src/holaMundo/HolaMundo.java
+++ src/holaMundo/HolaMundo.java
@@ -3,6 +3,9 @@ package holaMundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		// Hola mundo
 		System.out.println("¡Hola Mundo!");
+		// Nombre del desarrollador
+		System.out.println("Isidro Manuel Linares Camarena");
 	}
 }
